842e1ca93a22454df89d280169470f301b572770
Tom Chen <tchen25@ncsu.edu>
2023-08-22 21:14:05 -0400

    Micro xsate project
    learning xstate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { counterMachine } from './counterMachine';
+import { useMachine } from '@xstate/react';
+
 import './App.css';
 
 function App() {
+  const [state, send] = useMachine(counterMachine);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tom's first xstate counter</h1>
+      <h2>{state.context.count}</h2>
+      <button onClick={() => send('INCREMENT')}>Increment</button>
+      <button onClick={() => send('DECREMENT')}>Decrement</button>
     </div>
   );
 }
